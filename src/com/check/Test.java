676f6b47663cbab848fb43c90f24be76679049fd
Debasish Sahoo <debasishsahoo17@gmail.com>
2021-06-06 00:11:40 +0530

    first commit

---
+++ src/com/check/Test.java
@@ -0,0 +1,20 @@
+package com.check;
+
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry="yes";
+		if(hungry=="yes"){
+			System.out.println("Eat Something");		
+		}
+		else
+			System.out.println("plz do some work");	
+
+	}
+
+}
