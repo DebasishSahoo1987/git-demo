e207877273a01271c821381cc47b9a1b83103b3d
DebasishSahoo1987 <85211051+DebasishSahoo1987@users.noreply.github.com>
2021-06-06 10:04:12 +0530

    added additional checks

--- src/com/check/Test.java
+++ src/com/check/Test.java
@@ -9,7 +9,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String hungry="yes";
-		if(hungry=="yes"){
+		if(hungry=="yes" || hungry=="Y"){
 			System.out.println("Eat Something");		
 		}
 		else
